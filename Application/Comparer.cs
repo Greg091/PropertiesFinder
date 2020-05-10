e30ebaadfb668466d024729ae759f9d105fd77cf
Grzegorz <grzegorz.brzezinski@intel.com>
2020-05-10 09:06:23 +0200

    Implementacja Comparer'a

---
+++ Application/Comparer.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Application.Sample
+{
+    public class Comparer : IEqualityComparer<Entry>
+    {
+        public bool Equals(Entry x, Entry y)
+        {
+            if (GetHashCode(x) == GetHashCode(y))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetHashCode([DisallowNull] Entry obj)
+        {
+            var hashCode = $"{obj.PropertyDetails.NumberOfRooms}" +
+                $"{obj.PropertyAddress.City}" +
+                $"{obj.PropertyAddress.StreetName}" +
+                $"{obj.PropertyDetails.Area}" +
+                $"{obj.PropertyPrice.TotalGrossPrice}";
+            return hashCode.GetHashCode();
+        }
+    }
+}
